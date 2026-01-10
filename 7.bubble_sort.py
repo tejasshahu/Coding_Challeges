ecb1631e0fd6e137984efb379913e350d991bc37
tejasshahu <tejasshahu@yahoo.com>
2026-01-10 23:37:13 +0530

    Optimized Bubble sort.

--- 7.bubble_sort.py
+++ 7.bubble_sort.py
@@ -1,9 +1,13 @@
 def bubble_sort(a):
     n = len(a)
     for i in range(n-2,-1,-1):
+        is_swap = False
         for j in range(0,i+1):
             if a[j] < a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
+                is_swap = True
+        if is_swap is False:
+            break
     return a
 
 if __name__ == '__main__':
